1d5796765010a1f7a21e41ffa501c2a8e357b108
yourusernameglaubervilanegit config --global user.email glauberfmarques@gmail.comgit config --global user.name yourusernameglaubervilane <glauberfmarques@gmail.com>
2023-11-02 06:28:58 -0600

    Update the form assignment and add a thank you page to handle form submission

---
+++ scripts/renderFormData.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const urlParams = new URLSearchParams(window.location.search);
+
+  // Get values from the URL query parameters
+  const username = urlParams.get("username");
+  const email = urlParams.get("email");
+  const pageRating = urlParams.get("pageRating");
+  const timestamp = urlParams.get("timestamp");
+
+  // Populate the corresponding elements
+document.getElementById("username").textContent = username;
+document.getElementById("email").textContent = email;
+document.getElementById("pageRating").textContent = pageRating;
+document.getElementById("timestamp").textContent = timestamp;
+});
+
+
+function updateRatingValue(value) {
+  document.getElementById("ratingValue").textContent = value;
+}
+
+function addQueryParameters(event) {
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const pageRating = document.getElementById("pageRating").value;
+  const timestamp = new Date().toLocaleString();
+
+  document.getElementById("usernameHidden").value = username;
+  document.getElementById("emailHidden").value = email;
+  document.getElementById("pageRatingHidden").value = pageRating;
+  document.getElementById("timestampHidden").value = timestamp;
+}
