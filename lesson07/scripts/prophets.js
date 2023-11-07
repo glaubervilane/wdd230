dd1ba33c8a98b27e24c6ba176c3f48a79dbf431e
yourusernameglaubervilanegit config --global user.email glauberfmarques@gmail.comgit config --global user.name yourusernameglaubervilane <glauberfmarques@gmail.com>
2023-11-07 08:30:34 -0700

    Add prophets assignment page

---
+++ lesson07/scripts/prophets.js
@@ -0,0 +1,36 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  displayProphets(data.prophets);
+}
+
+getProphetData();
+
+const displayProphets = (prophets) => {
+  prophets.forEach((prophet) => {
+    let card = document.createElement('section');
+    let fullName = document.createElement('h2');
+    let portrait = document.createElement('img');
+    let birthInfo = document.createElement('p');
+
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '340');
+    portrait.setAttribute('height', '440');
+
+    // Display birthdate and birthplace on separate lines
+    birthInfo.innerHTML = `Birth Date: ${prophet.birthdate}<br>Birthplace: ${prophet.birthplace}`;
+
+    card.appendChild(fullName);
+    card.appendChild(portrait);
+    card.appendChild(birthInfo);
+
+    cards.appendChild(card);
+  });
+};
+
