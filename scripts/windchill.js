e8e970bf48abdcf1b633a2974d5518fa51444deb
yourusernameglaubervilanegit config --global user.email glauberfmarques@gmail.comgit config --global user.name yourusernameglaubervilane <glauberfmarques@gmail.com>
2023-11-14 07:41:55 -0700

    Update windchill js file to use OpenWeather API

--- scripts/windchill.js
+++ scripts/windchill.js
@@ -1,64 +1,73 @@
-/* code in javascript of weather with openweathermap============  */
+// Select HTML elements
+const temperatureSpan = document.querySelector('#temperature');
+const windSpeedSpan = document.querySelector('#wind-speed');
+const windChillSpan = document.querySelector('#wind-chill');
 
+// Declare URL variable
+const url = 'https://api.openweathermap.org/data/2.5/weather';
 
-window.addEventListener('load', async () => {
-  const latitude = 51.0447;
-  const longitude = -114.0719;
+// Define asynchronous function named "apiFetch()"
+async function apiFetch() {
+  // Securely handle API key in a browser environment
+  const apiKey = window.WEATHER_API_KEY;
 
-  const temperatureSpan = document.querySelector('#temperature');
-  const windSpeedSpan = document.querySelector('#wind-speed');
-  const windChillSpan = document.querySelector('#wind-chill');
+  // Ensure that the API key is provided
+  if (!apiKey) {
+    console.error("API Key is missing.");
+    return;
+  }
 
-  const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=' + latitude + ',' + longitude;
-
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '4edddb0f16mshdf57012d8fe67ffp1583d3jsnb5caa4023c12',
-      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-    }
-  };
+  const city = 'Calgary';
+  const countryCode = 'CA';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${apiKey}&units=imperial`;
 
   try {
-    const response = await fetch(url, options);
-    const data = await response.json();
+    const response = await fetch(url);
 
-    // Extract actual temperature in Fahrenheit
-    const actualTemperature = data.current.temp_f;
-
-    // Update the temperature span with the actual temperature
-    temperatureSpan.textContent = actualTemperature.toFixed(2) + '°F';
-
-    // Extract temperature in Celsius and wind speed in m/s
-    const temperatureCelsius = data.current.temp_c;
-    const windSpeedMS = data.current.wind_kph / 3.6;
-
-    // Convert temperature to Fahrenheit
-    const temperatureFahrenheit = (temperatureCelsius * 9/5) + 32;
-
-    // Convert wind speed to miles per hour
-    const windSpeedMPH = windSpeedMS * 2.237;
-
-    // Update the temperature and wind speed spans
-    temperatureSpan.textContent = temperatureFahrenheit.toFixed(2) + '°F';
-    windSpeedSpan.textContent = windSpeedMPH.toFixed(2) + ' mph';
-
-    // Calculate the wind chill
-    const windChillValue = calculateWindChill(temperatureFahrenheit, windSpeedMPH);
-
-    // Display wind chill or "N/A"
-    if (temperatureFahrenheit <= 50 && windSpeedMPH > 3.0) {
-      windChillSpan.textContent = 'Wind Chill: ' + windChillValue.toFixed(2) + '°F';
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data);
     } else {
-      windChillSpan.textContent = 'Wind Chill: N/A';
+      throw Error(await response.text());
     }
   } catch (error) {
     console.error(error);
   }
-});
+}
+
+apiFetch();
+
+// Invoke the apiFetch() function
+apiFetch();
+
+// Build the displayResults function
+function displayResults(data) {
+  // Display temperature in Fahrenheit
+  temperatureSpan.innerHTML = `${data.main.temp.toFixed(2)}&deg;F`;
+
+  // Display wind speed in mph
+  windSpeedSpan.innerHTML = `${data.wind.speed.toFixed(2)}`;
+
+  // Set weather icon and description
+  const weatherIcon = document.getElementById('weather-icon');
+  const weatherDescription = document.getElementById('weather-description');
+  weatherIcon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  weatherIcon.alt = data.weather[0].description;
+  weatherDescription.innerHTML = data.weather[0].description;
+
+  // Calculate wind chill
+  const windChillValue = calculateWindChill(data.main.temp, data.wind.speed);
+
+  // Display wind chill or "N/A"
+  if (data.main.temp <= 50 && data.wind.speed > 3.0) {
+    windChillSpan.innerHTML = `Wind Chill: ${windChillValue.toFixed(2)}&deg;F`;
+  } else {
+    windChillSpan.innerHTML = 'Wind Chill: N/A';
+  }
+}
 
 // Function to calculate wind chill
 function calculateWindChill(temperatureFahrenheit, windSpeedMPH) {
-  const windChill = 35.74 + (0.6215 * temperatureFahrenheit) - 35.75 * Math.pow(windSpeedMPH, 0.16) + 0.4275 * temperatureFahrenheit * Math.pow(windSpeedMPH, 0.16);
+  const windChill = 35.74 + 0.6215 * temperatureFahrenheit - 35.75 * Math.pow(windSpeedMPH, 0.16) + 0.4275 * temperatureFahrenheit * Math.pow(windSpeedMPH, 0.16);
   return windChill;
 }
