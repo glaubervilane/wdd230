19f13ef088035bffb2d501943183bc6261f7fdb2
yourusernameglaubervilanegit config --global user.email glauberfmarques@gmail.comgit config --global user.name yourusernameglaubervilane <glauberfmarques@gmail.com>
2023-12-02 15:18:35 -0700

    Improve styles from modal window in Rentals page

--- final-project/scripts/modal-rentals.js
+++ final-project/scripts/modal-rentals.js
@@ -1,7 +1,41 @@
+// Rental details object
+const rentalDetails = {
+  type1: {
+    name: 'Honda Metro Scooter',
+    image: './images/honda-metro.jpg',
+    description: '49cc liquid-cooled single-cylinder four-stroke.'
+  },
+  type2: {
+    name: 'Honda Dio Scooter',
+    image: './images/honda-dio.jpg',
+    description: '109.51 cc air-cooled engine which produces 7.85 PS @ 8000 rpm of power.'
+  },
+  type3: {
+    name: 'Honda PCX150 Scooter',
+    image: './images/honda-pcx.jpg',
+    description: '156.9cc liquid-cooled 80º single-cylinder four-stroke.'
+  },
+  type4: {
+    name: 'Honda Pioneer ATV',
+    image: './images/honda-pioneer.jpg',
+    description: '999cc liquid-cooled longitudinally mounted parallel-twin four-stroke.'
+  },
+  type5: {
+    name: 'Jeep Wrangler 4 doors with a/c',
+    image: './images/jeep-wrangler1.jpg',
+    description: 'ERC Regular Unleaded V-6, 3.6 L/220, Sequential MPI.'
+  },
+  type6: {
+    name: 'Jeep Wrangler - 2 door',
+    image: './images/jeep-wrangler2.jpg',
+    description: '3.6 Liters, V-6, 285 Hp, 6,400, Four-Wheel.'
+  },
+};
+
 document.addEventListener('DOMContentLoaded', function () {
   const modal = document.querySelector('.modal');
-  const closeModalBtn = document.querySelector('.close-btn');
-  const detailsBtns = document.querySelectorAll('.details-btn');
+  const closeModalBtn = document.querySelector('.modal .close-btn');
+  const detailsBtns = document.querySelectorAll('.btn-card');
 
   function setupEventListeners() {
     detailsBtns.forEach(btn => {
@@ -11,10 +45,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const modalContent = document.querySelector('.modal-content');
         modalContent.innerHTML = `
-              <img src="${details.image}" alt="${details.name}">
-              <h2>${details.name}</h2>
-              <p>${details.description}</p>
-          `;
+          <img src="${details.image}" alt="${details.name}">
+          <h2>${details.name}</h2>
+          <p>${details.description}</p>
+        `;
 
         modal.style.display = 'block';
       });
@@ -34,8 +68,8 @@ document.addEventListener('DOMContentLoaded', function () {
   // Check if elements are present, wait for them to be added if not
   if (!modal || !closeModalBtn || !detailsBtns.length) {
     const observer = new MutationObserver(function () {
-      const newCloseModalBtn = document.querySelector('.close-btn');
-      const newDetailsBtns = document.querySelectorAll('.details-btn');
+      const newCloseModalBtn = document.querySelector('.modal .close-btn');
+      const newDetailsBtns = document.querySelectorAll('.btn-card');
       if (modal && newCloseModalBtn && newDetailsBtns.length) {
         observer.disconnect();
         setupEventListeners();
@@ -47,17 +81,3 @@ document.addEventListener('DOMContentLoaded', function () {
     setupEventListeners();
   }
 });
-
-// Sample rental details object (replace with your actual data)
-const rentalDetails = {
-  type1: {
-    name: 'Rental Type 1',
-    image: './images/honda-metro.jpg',
-    description: 'Details about Honda Metro Scooter.'
-  },
-  type2: {
-    name: 'Rental Type 2',
-    image: './images/honda-dio.jpg',
-    description: 'Details about Honda Dio Scooter.'
-  },
-};
